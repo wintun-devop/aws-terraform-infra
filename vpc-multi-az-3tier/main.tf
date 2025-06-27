a264d5f022e0f25994eef7d9883f43de5e6270ef
wintun-devop <wintun.hlaing@outlook.com>
2025-06-27 21:28:05 +0630

    update 3tier with nat gw

--- vpc-multi-az-3tier/main.tf
+++ vpc-multi-az-3tier/main.tf
@@ -52,12 +52,12 @@ resource "aws_subnet" "public-subnet-zb-4" {
   }
 }
 
-resource "aws_subnet" "api-subnet-zb-4" {
+resource "aws_subnet" "api-subnet-zb-5" {
   vpc_id            = aws_vpc.tf-test-vpc.id
   cidr_block        = var.api-subnet-zb-5
   availability_zone = var.az2
   tags = {
-    Name = "api-subnet-zb-4"
+    Name = "api-subnet-zb-5"
   }
 }
 
@@ -90,13 +90,26 @@ resource "aws_subnet" "api-subnet-zc-8" {
 
 resource "aws_subnet" "private-subnet-zc-9" {
   vpc_id            = aws_vpc.tf-test-vpc.id
-  cidr_block        = var.api-subnet-zc-8
+  cidr_block        = var.private-subnet-zc-9
   availability_zone = var.az3
   tags = {
     Name = "private-subnet-zc-9"
   }
 }
 
+
+/*  */
+resource "aws_eip" "tf-test-vpc-nat-ip" {
+  tags = {
+    Name = "tf-test-vpc-nat-ip"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-tf-test-vpc" {
+  allocation_id = aws_eip.tf-test-vpc-nat-ip.id
+  subnet_id = aws_subnet.public-subnet-za-1.id
+
+}
 
 resource "aws_route_table" "tf-test-public-rt" {
   vpc_id = aws_vpc.tf-test-vpc.id
@@ -112,6 +125,11 @@ resource "aws_route_table" "tf-test-public-rt" {
 
 resource "aws_route_table" "tf-test-api-rt" {
   vpc_id = aws_vpc.tf-test-vpc.id
+  /* outbound access to internet */
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gw-tf-test-vpc.id
+  }
   tags = {
     Name = "tf-test-api-rt"
   }
@@ -138,7 +156,7 @@ resource "aws_route_table_association" "api-association" {
   route_table_id = aws_route_table.tf-test-api-rt.id
   for_each = {
     subnet-1 = aws_subnet.api-subnet-za-2.id
-    subnet-2 = aws_subnet.api-subnet-zb-4.id
+    subnet-2 = aws_subnet.api-subnet-zb-5.id
     subnet-3 = aws_subnet.api-subnet-zc-8.id
   }
   subnet_id = each.value
